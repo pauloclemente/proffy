76bd670f6d60230a780fe6e102d8364b4aad3117
Paulo Barros <clementedbarros@gmail.com>
2020-08-04 19:09:35 -0300

    feat: add create classes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,8 @@
-import express from 'express';
+import express from 'express'
+import routes from './routes'
+
 const app = express();
+app.use(express.json())
 
-
-app.get('/users', (request, response) => {
-  return response.json({message: 'Hello Word'})
-  
-})
-app.listen(3333);
+app.use(routes)
+app.listen(3333)
