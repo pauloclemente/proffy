656360dca541ac8c2dfa5a64b3f787f8accdc2e8
Paulo Barros <clementedbarros@gmail.com>
2020-08-09 19:37:28 -0300

    feat: add landing page

---
+++ mobile/App.tsx
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import Landing from './src/pages/Landing';
+import { AppLoading } from 'expo';
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+
+export default function App() {
+  let [frontsLoaded] = useFonts({
+    Archivo_400Regular, 
+    Archivo_700Bold, 
+    Poppins_400Regular, 
+    Poppins_600SemiBold,
+  });
+  if(!frontsLoaded){
+    return <AppLoading/>
+  } else {
+    return (
+      <>
+        <Landing/>
+        <StatusBar style="light" />
+      </>
+      
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
